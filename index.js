5b73eb52c426e4e19418c5d72d45e27c6a3dfbcd
ryanbas21 <ryanbas21@gmail.com>
2017-04-01 09:18:14 -0400

    saving added function description/jsdoc

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
+ /**
+ *  Compose Function
+ *  @param  {...fns} fns [array of functions]
+ *  @return {function} [function which reduces
+ *  the array of functions, applying the result of the last applied
+ *  function to the next]
+ */
+ const compose = (...args) => x => args.reduceRight((acc, fn) => fn(acc), x);
 
-// compose(...fns: [...Function]) => Function
-const compose = (...args) => x => args.reduceRight( (acc, fn) => fn(acc), x);
 
-//pipe(...fns: [...Function]) => Function
-const pipe = (...args) => x => args.reduce((args, fn) => fn(acc), x);
+ /**
+ *  Pipe Function
+ *  @param  {...fns} fns turns arguments into an array of functions
+ *  @return {[type]} [description]
+ */
+ const pipe = (...args) => x => args.reduce((acc, fn) => fn(acc), x);
 
-// trace(label: Any) => (value: v) => v, effects(log label:value to console)
-const trace = label => value => {
-  console.log(`${label}: ${value}`);
-  return value;
-};
 
-// add2(a) => b => Number
-const add = a => b => a + b;
+/**
+ *  Trace Function
+ *  @param  {string} label description of value
+ *  @return { function } [returns a function that logs out a string label:value
+ *  and then returns the value]
+ */
+ const trace = label => (value) => {
+   console.log(`${label}: ${value}`);
+   return value;
+ };
 
-//increment
-const increment = add(1);
+/**
+ *  [add2 function]
+ *  @param  {number} a a number
+ *  @return {functions} takes a number and returns out the result of adding the two
+ */
+const add2 = a => b => a + b;
 
-//
+/**
+ *  [increment description]
+ *
+ *  @type {function}
+ */
+const increment = add2(1);
